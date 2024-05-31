1983ce3c202d3bb928cfe08bfe294a0af0408a6d
LeandroFGA1 <aucache.leandro@gmail.com>
2024-05-31 16:30:07 -0300

    fix router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -19,6 +19,7 @@ function Router() {
             <Route path='/services' element={<Services/>}/>
             <Route path='/aboutUs'  element={<AboutUs/>}/>
             <Route path='/contact'  element={<Contact/>}/>
+            <Route path='*' element={<Navigate to="/" replace />} />
         </Routes>
     )
 }
